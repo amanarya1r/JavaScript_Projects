cc7b2f2df41779a3453c776a36c1ac220bbf05fd
yuy(w)@amanarya1r <amanarya1r@gmail.com>
2025-01-01 19:55:31 +0530

    readme tweaks with other comments are added

--- Slot_Machine/readme.md
+++ Slot_Machine/readme.md
@@ -1,5 +1,11 @@
+## Slot Machine 🎰
+Slot Machine is a js script which created on the simple logic, loops and conditional statement. The code can be further improved and new can features can be added. 
+
 ## How slot machine🎰 works? 
-We there will be three line like this 
+Every time script runs, user will be asked to enter a certain ammount, then user will be asked to bet on number of lines, then user will be asked to enter the nubmer of lines to bet on.
+After that script will run in synchronus manner and slot matching will be done for if user win/loose, user would be asked if user want to play again if user write yes then same loop will happen again and if user write no then the user breaks the loop and exit the script.
+
+**Here is how Slot Machine🎰 mechanism like:** 
 
 | A | A | A | 
 |---|---|---|
@@ -14,3 +20,45 @@ So there will be three rows, from one of which a user can win if there will be t
 | C | C | A |
 
 So if a user bet on a row line for a certain ammount then it get multiplied as per the row line(suppose if 1 row line is selected than it would be multiplied for once, if two then it would be mutliplied for twice and if three then it would be multiplied for thrice).
+
+This code will transpose the matrix:
+```javascript
+const transpose = (reels) => { //transposing of 2d array{matrix} done so that we can match the data easily
+
+// [[A B C], [D D D], [A A A]]
+// [A D A]
+// [B D A]
+// [C D A]
+
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++){
+        rows.push([]); //a new row get pushed because for every single row an array is needed to represent it
+        for (let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i]); //j is column index and i is row index
+        }
+    }
+
+    return rows;
+}
+```
+
+And the rows and column get printed using this code:
+```javascript
+const printRows = (rows) => {
+    for(const row of rows) {
+        let rowString = ""; // A | B | D   <= this will look like this
+        for (const [i, symbol] of row.entries()){
+            rowString += symbol;
+            if(i != row.length - 1){ //this will add pipe before starting and end of rowstring array
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+    }
+}
+
+```
+
+**Here is how Slot Machine works:**
+
--- Slot_Machine/slot_machine.js
+++ Slot_Machine/slot_machine.js
@@ -1,19 +1,19 @@
 const prompt = require("prompt-sync")();
 
-const ROWS = 3;
+const ROWS = 3; // all these variable are constant thats why they are written in capital leter
 const COLS = 3;
 
 const SYMBOLS_COUNT = {
-    "A": 2,
+    "A": 2, //A will appear twice in a slot wheel
     "B": 4, 
     "C": 6,
-    "D": 8
+    "D": 8 //D will appear 8times in a slot wheel
 }
 
-const SYMBOLS_VALUES = {
-    "A": 5,
+const SYMBOLS_VALUES = { 
+    "A": 5, //If line of A appear, the bet will multiplied with 5
     "B": 4, 
-    "C": 3,
+    "C": 3, //If line of C appear, the bet will multiplied with 3
     "D": 2
 }
 
@@ -43,6 +43,7 @@ const getNumberOfLines = () => {
     }
 };
 
+//bet will be done on the basis of number of lines
 const getBet = (balance, lines) => {
     while (true) {
         const bet = prompt("Enter the bet per line: ");
@@ -56,29 +57,35 @@ const getBet = (balance, lines) => {
     }
 };
 
-const spin = () => {
+const spin = () => { //its spining the slot whell[Randomly selecting SELECT_COUNT values on each wheel and stopping at a certain point]
     const symbols = [];
-    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) { //symbol gives keys inside SYMBOLS_COUNT object and count will give values associated to there keys
         for (let i = 0; i < count; i++){
-            symbols.push(symbol);
+            symbols.push(symbol); //symbols from SYMBOLS_COUNT are added to symbols array
         }
     }
     const reels = [];
-    for (let i = 0; i < COLS; i++){
+    for (let i = 0; i < COLS; i++){ //this will generate columns for slot machine wheel and it get generated for each reel
             reels.push([]);
-            const reelSymbols = [...symbols]; //this will copy symbols as it is
-        for (let j = 0; j < ROWS; j++){
-            const randomIndex = Math.floor(Math.random()*reelSymbols.length);
-            const selectedSymbol = reelSymbols[randomIndex];
-            reels[i].push(selectedSymbol);
-            reelSymbols.splice(randomIndex, 1);
+            const reelSymbols = [...symbols]; //this will copy symbols we have available to choose from reels into array
+            //using this because for every single slot wheel these are all the available symbols slot machine have
+            // So the symbol get randomly selected and get removed from the available symbol for the individual wheel and this process continue
+
+        for (let j = 0; j < ROWS; j++){ //this will pick 3 elements from number of rows slot machine have
+            const randomIndex = Math.floor(Math.random()*reelSymbols.length); //randomIndex choosen randomly 
+            //Math.random() will selecting between value 0 to 1.
+            //Mathrandom()*reelSymbols.length maximus possible number would be how many symbols we have
+            //Math.floor will round down
+            const selectedSymbol = reelSymbols[randomIndex]; 
+            reels[i].push(selectedSymbol); //selectedSymbol added 
+            reelSymbols.splice(randomIndex, 1); //added symbol get removed from reelSymbols
         }
     }
 
     return reels;
 };
 
-const transpose = (reels) => {
+const transpose = (reels) => { //transposing of 2d array{matrix} done so that we can match the data easily
     const rows = [];
 
     for (let i = 0; i < ROWS; i++){
